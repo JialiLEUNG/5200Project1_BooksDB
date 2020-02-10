cdaf477dcd868e280c04b47c0eace18bc3cdb9b9
Jiali <hcloud@Jianyuns-MacBook-Air.local>
2020-02-09 19:35:53 -0800

    add sql

--- src/BooksPopulate.java
+++ src/BooksPopulate.java
@@ -1,3 +1,5 @@
+import com.mysql.cj.protocol.Resultset;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,16 +7,16 @@ import java.io.IOException;
 import java.sql.*;
 
 public class BooksPopulate {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         // Four tables will be created in the current program:
         // Schema of the four tables can be seen in E/R diagram in the report.
         String dbTables[] = {
-                "Authors", "Titles", "Publishers", // entities
-                "AuthorISBN" // relation (Authors and Titles is many-to-many relationship.)
+                "AuthorISBN", // relation (Authors and Titles is many-to-many relationship.)
+                "Authors", "Titles", "Publishers" // entities
         };
 
         // name of the Books data file
-        String fileName = "src/BookData.txt";
+        String fileName = "src/BookDataNEW.txt";
 
         // connect to database
         Connection conn = getConnection();
@@ -42,8 +44,8 @@ public class BooksPopulate {
                     "INSERT INTO Publishers(publisherID, publisherName) values(?,?)"
             ); // publisherID, publisherName
             PreparedStatement insertRow_Titles = conn.prepareStatement(
-                    "INSERT INTO Titles(isbn, years, publisherID, price, title) values(?,?,?,?,?,?)"
-            ); // isbn, years, publisherID, price, title
+                    "INSERT INTO Titles(editionNumber, years, publisherID, price, title, isbn) values(?,?,?,?,?,?)"
+            ); // editionNumber, years, publisherID, price, title, isbn
             PreparedStatement insertRow_AuthorISBN = conn.prepareStatement(
                     "INSERT INTO AuthorISBN(authorID, isbn) values(?,?)"
             ) // authorID, isbn
@@ -57,7 +59,9 @@ public class BooksPopulate {
                     stmt.executeUpdate("DELETE FROM " + tbl);
                     System.out.println("Deleting existing records in table" + tbl + " succeeded.");
                 } catch(SQLException e){
+                    System.out.println(e.getMessage());
                     System.err.println("Deleting existing records in table" + tbl + " failed.");
+                    throw new Exception(e.getMessage());
                 }
             }
 
@@ -85,8 +89,10 @@ public class BooksPopulate {
                     insertRow_Authors.executeUpdate();
                 } catch(SQLException e){
                     // tuple already exists
-                    System.err.println(String.format("Table Authors: Already inserted authorID %d firstName %s lastName %s",
-                            authorID, firstName, lastName));
+//                    System.out.println(e.getMessage());
+//                    System.err.println(String.format("Table Authors: Already inserted authorID %d firstName %s lastName %s",
+//                            authorID, firstName, lastName));
+//                     throw new Exception(e.getMessage());
                 }
 
                 /** get values from rowArr for the Publishers table */
@@ -99,45 +105,84 @@ public class BooksPopulate {
                     insertRow_Publishers.setString(2, publisherName);
                     insertRow_Publishers.executeUpdate();
                 } catch(SQLException e){
-                    System.err.println(String.format("Table Publishers: Already inserted publisherID %d publisherName %s", publisherID, publisherName));
+//                    System.out.println(e.getMessage());
+//                    System.err.println(String.format("Table Publishers: Already inserted publisherID %d publisherName %s", publisherID, publisherName));
+//                    throw new Exception(e.getMessage());
                 }
 
                 /** get values from rowArr for the Titles table */
-                int editionName = Integer.parseInt(rowArr[5]);
+
+                int editionNumber = Integer.parseInt(rowArr[5]);
                 String years = rowArr[6];
                 float price = Float.parseFloat(rowArr[7]);
                 String title = rowArr[8];
+                String isbn = rowArr[9];
                 // add Titles tuple if does not exist
                 try{
-                    insertRow_Titles.setInt(1, editionName);
+
+                    insertRow_Titles.setInt(1, editionNumber);
                     insertRow_Titles.setString(2, years);
                     insertRow_Titles.setInt(3, publisherID);
                     insertRow_Titles.setFloat(4, price);
                     insertRow_Titles.setString(5, title);
+                    insertRow_Titles.setString(6, isbn);
                     insertRow_Titles.executeUpdate();
                 } catch(SQLException e){
-                    System.err.println(String.format("Table Titles: Already inserted editionName %d years %s publisherID %d price %f title %s",
-                            editionName, years, publisherID, price, title));
+//                    System.err.println(String.format("Table Titles: Already inserted editionName %d years %s publisherID %d price %f title %s",
+//                            editionNumber, years, publisherID, price, title));
+//                     throw new Exception(e.getMessage());
                 }
 
                 /** get values from rowArr for the AuthorISBN table */
-                String isbn = rowArr[9];
                 // add AuthorISBN tuple if does not exist
                 try{
                     insertRow_AuthorISBN.setInt(1, authorID);
                     insertRow_AuthorISBN.setString(2, isbn);
                     insertRow_AuthorISBN.executeUpdate();
                 } catch (SQLException e){
-                    System.err.println(String.format("Table AuthorISBN: Already inserted authorID %d isbn %s",
-                            authorID, isbn));
+//                    System.err.println(String.format("Table AuthorISBN: Already inserted authorID %d isbn %s",
+//                            authorID, isbn));
+//                    throw new Exception(e.getMessage());
                 }
             }
+
+            // Print the articles that are published after 2000 in the Titles table
+            System.out.println("=============== articles published after 2000 in table Titles================");
+            getArticleAfter2000(stmt);
+            // Print the articles that are published by the same publisher.
+//            getArticleByPublisher(stmt);
+            // order book by price in table Titles
+            System.out.println("=============== order book by price in table Titles ================");
+            orderBookByPrice(stmt);
+
+            // count the number of books by publisherID in table Titles
+            System.out.println("=============== count the number of books by publisher in table Titles ================");
+            countISBNbyPublisher(stmt);
+
+            // books start with 'Harry Potter'
+            System.out.println("=============== books whose title starts with 'Harry Potter' in table Titles ================");
+            bookStartByHarryPotter(stmt);
+
+
+            // find authors of 'Bitcoin's Academic Pedigree'
+            System.out.println("=============== authors of 'Bitcoin's Academic Pedigree' ================");
+            findAuthorByBookTitle(stmt);
+
+            // find the book written by Neil Savage
+            System.out.println("=============== find Neil Savage's book ================");
+            findNeilSavageBooks(stmt);
+
+            // average price by each publisher
+            System.out.println("=============== calculate average price by each publisher ==================");
+            avgPriceByPublisher(stmt);
+
         } catch (IOException | SQLException e) {
             e.printStackTrace();
+            throw new Exception(e.getMessage());
         }
     }
 
-    public static Connection getConnection(){
+    public static Connection getConnection() throws Exception {
         try{
             // The connection URL for the mysql database is jdbc:mysql://localhost:3306/Books
             // where jdbc is the API,
@@ -157,23 +202,26 @@ public class BooksPopulate {
             return conn;
         } catch(Exception e){
             System.out.println(e);
+            throw new Exception(e.getMessage());
         }
-        return null;
+
     }
 
-    public static void dropTable(Connection conn, String[] dbTables){
+    public static void dropTable(Connection conn, String[] dbTables) throws SQLException {
         for (String tbl:dbTables){
             try{
                 Statement stmt = conn.createStatement();
                 stmt.executeUpdate("drop table " + tbl);
                 System.out.println("Dropped table "+ tbl);
             } catch (SQLException e){
-                System.out.println("Did not drop table " + tbl);
+                //System.out.println("Did not drop table " + tbl);
+                System.out.println(e.getMessage());
+                throw new SQLException(e.getMessage());
             }
         }
     }
 
-    public static void createTable(Connection conn){
+    public static void createTable(Connection conn) throws Exception {
         try{
             // create the Authors table
             PreparedStatement createTable_Authors = conn.prepareStatement(
@@ -198,12 +246,12 @@ public class BooksPopulate {
             // create the Titles table
             PreparedStatement createTable_Titles = conn.prepareStatement(
                     "CREATE TABLE IF NOT EXISTS Titles(" +
-                            "isbn CHAR(10) NOT NULL," +
                             "editionNumber int NOT NULL," +
                             "years CHAR(4) NOT NULL," +
                             "publisherID int NOT NULL," +
                             "price NUMERIC (8, 2) NOT NULL," +
                             "title varchar(500) NOT NULL," +
+                            "isbn CHAR(10) NOT NULL," +
                             "PRIMARY KEY(isbn)," +
                             "FOREIGN KEY(publisherID) REFERENCES Publishers(publisherID))");
             createTable_Titles.executeUpdate();
@@ -223,6 +271,7 @@ public class BooksPopulate {
 
         } catch(SQLException e){
             e.printStackTrace();
+            throw new Exception(e.getMessage());
         }
 
     }
@@ -241,4 +290,182 @@ public class BooksPopulate {
             System.out.println("insert completed");
         }
     }
+
+    // get information
+    // getArticleAfter2000 prints the articles that are published after 2000 in the Titles table
+    public static void getArticleAfter2000(Statement stmt) throws SQLException {
+        // result set for queries
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT title, years " +
+                    "FROM Titles " +
+                    "Where years > 2000");
+            while (rs.next()){
+                String articleName = rs.getString(1);
+                int year = rs.getInt(2);
+                System.out.printf(String.format("Table Title: article after Year 2000 is %s (year: %d).\n", articleName, year));
+            }
+            System.out.println("Table Title: No more article after Year 2000");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void getArticleByPublisher(Statement stmt) throws SQLException {
+        // result set for queries
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT Titles.title, Publishers.publisherName " +
+                    "FROM Titles" +
+                    "NATURAL JOIN Publishers on Titles.publisherID = Publishers.publisherID" +
+                    "GROUP BY Titles.publisherID");
+            while (rs.next()){
+                String articleName = rs.getString(1);
+                String publisherName = rs.getString(2);
+                System.out.printf(String.format("Table Title: article by %s is %s.\n", publisherName, articleName));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * order the book by price in ascending order
+     * @param stmt
+     * @throws Exception
+     */
+    public static void orderBookByPrice(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT title, price " +
+                    "FROM Titles " +
+                    "ORDER BY price ASC");
+            while (rs.next()){
+                String articleName = rs.getString(1);
+                Float price = rs.getFloat(2);
+                System.out.printf(String.format("Table Title: price of article %s is %f.\n", articleName, price));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * count the number of books by publisher
+     * @param stmt
+     * @throws Exception
+     */
+    public static void countISBNbyPublisher(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT publisherID, count(isbn) " +
+                    "FROM Titles " +
+                    "GROUP BY publisherID");
+            while (rs.next()){
+                int publisherID = rs.getInt(1);
+                int bookCount = rs.getInt(2);
+                System.out.printf(String.format("Table Title: Number of books by Publisher %d is %d.\n", publisherID, bookCount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * books with title starting with 'Harry Potter';
+     * @param stmt
+     * @throws Exception
+     */
+    public static void bookStartByHarryPotter(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT title " +
+                    "FROM Titles " +
+                    "WHERE title like 'Harry Potter%'");
+            while (rs.next()){
+                String harryBook = rs.getString(1);
+                System.out.printf(String.format("Table Title: Book start with 'Harry Potter': %s.\n", harryBook));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void findAuthorByBookTitle(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT firstName, lastName " +
+                    "FROM Titles, Authors, AuthorISBN " +
+                    "WHERE Authors.authorID = AuthorISBN.authorID " +
+                    "      AND Titles.isbn = AuthorISBN.isbn" +
+                    "      AND Titles.title = 'Bitcoin''s Academic Pedigree'");
+            while (rs.next()){
+                String firstName = rs.getString(1);
+                String lastName = rs.getString(2);
+                System.out.printf(String.format("The author(s) of Bitcoin''s Academic Pedigree: %s %s.\n", firstName, lastName));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * find books written by Nail Savage
+     * @param stmt
+     * @throws Exception
+     */
+    public static void findNeilSavageBooks(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT Titles.title " +
+                    "FROM Titles, Authors, AuthorISBN " +
+                    "WHERE Authors.authorID = AuthorISBN.authorID " +
+                    "      AND Titles.isbn = AuthorISBN.isbn" +
+                    "      AND Authors.firstName = 'Neil'" +
+                    "      AND Authors.lastName = 'Savage'");
+            while (rs.next()){
+                String title = rs.getString(1);
+                System.out.printf(String.format("Neil Savage's book: %s.\n", title));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * calculate the average price for books by each publisher
+     * @param stmt
+     * @throws Exception
+     */
+    public static void avgPriceByPublisher(Statement stmt) throws Exception{
+        ResultSet rs = null;
+        try{
+            rs = stmt.executeQuery("" +
+                    "SELECT Titles.publisherID, Publishers.publisherName, round(avg(Titles.price),2) " +
+                    "FROM Titles, Publishers " +
+                    "WHERE Titles.publisherID = Publishers.publisherID " +
+                    "GROUP BY Titles.publisherID");
+            while (rs.next()){
+                int publisherID = rs.getInt(1);
+                String publisherName = rs.getString(2);
+                Float price = rs.getFloat(3);
+                System.out.printf(String.format("Average price by Publisher %d (%s): %f.\n", publisherID, publisherName, price));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
